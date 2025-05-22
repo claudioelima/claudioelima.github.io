d1e4d5e19c597f72fc1fba14093c41ca730a2707
Claudio Lima <32175551+claudioelima@users.noreply.github.com>
2025-05-22 10:43:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyD3H5u78DAsCvnvI0gzqGAbvYycZlmzSfU",
+  authDomain: "portfolio-academico-74f3f.firebaseapp.com",
+  projectId: "portfolio-academico-74f3f",
+  storageBucket: "portfolio-academico-74f3f.firebasestorage.app",
+  messagingSenderId: "355320485565",
+  appId: "1:355320485565:web:12a560937c49ff72edc9d4"
+};
+
 function verProjeto(url) {
   window.open(url, '_blank');
 }
